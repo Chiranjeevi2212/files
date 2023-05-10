e5ec7e19fac0050a60315e113e508593c0880a70
Chiranjeevi2212 <129724287+Chiranjeevi2212@users.noreply.github.com>
2023-05-10 12:46:53 +0530

    Add files via upload

---
+++ src/main/java/pkg1/MyCalc.java
@@ -0,0 +1,28 @@
+package pkg1;
+
+public class MyCalc {	
+	public int sum(int a, int b)
+	{
+		return (a+b);
+	}
+	public int diff(int a, int b)
+	{
+		return (a-b);
+	}
+	public int mul(int a, int b)
+	{
+		return (a*b);
+	}
+	public int div(int a, int b)
+	{
+		return (a/b);
+	}	
+	public int rem(int a, int b)
+	{
+		return (a%b);
+	}
+	public void show()
+	{
+		System.out.println("Checking auto build");;
+	}
+}
